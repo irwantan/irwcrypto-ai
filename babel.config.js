2ff9346c1365bc22982f52121a903f39701e806d
irwantan <32396291+irwantan@users.noreply.github.com>
2026-06-13 09:23:28 +0700

    Fix: Remove invalid .plugins property from react-native-reanimated/plugin configuration

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'nativewind/babel',
-      ['react-native-reanimated/plugin', { relativeSourceLocation: true }],
+      'react-native-reanimated/plugin',
     ],
   };
 };
