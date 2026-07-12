db7492dcb94c89c95db6758c10c6054b1367e84a
Entity Hub Networking <entityhubnetwordking@outlook.com>
2026-07-12 20:13:30 +0000

    fix: remove nativewind/babel plugin (NW v4 uses CSS interop), add withCSSInterop to metro config
    NativeWind v4 dropped the Babel plugin. The old 'nativewind/babel' entry
    caused '.plugins is not a valid Plugin property' error during JS bundling.
    Now using react-native-css-interop/metro withCSSInterop instead.

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const { withCSSInterop } = require("react-native-css-interop/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+module.exports = withCSSInterop(config);
